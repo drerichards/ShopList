ebcf7c90c56d901d0584adf354ed2804962e9b01
Andre <elij.richards22@gmail.com>
2016-06-28 13:30:00 -0400

    mobile commit

--- js/jquery.js
+++ js/jquery.js
@@ -1,59 +1,61 @@
-$(document).ready(function () {
+$(document).ready(function() {
 
-// gonna use enter as well as button for user conveniency
-  $('#textbox').keyup(function(event){
-    if(event.keyCode == 13) {
-      event.preventDefault();
-      $('.entry-btn').click();
-    };
-  }); 
+    // gonna use enter as well as button for user conveniency
+    $('#textbox').keyup(function(event) {
+        if (event.keyCode == 13) {
+            event.preventDefault();
+            $('.entry-btn').click();
+        };
+    });
 
-// add item to list
-  $('.entry-btn').click(function() {
-    var txtbox = document.getElementById('textbox');
-    var txtval = txtbox.value;
-    event.preventDefault();
+    // add item to list
+    $('.entry-btn').click(function() {
+        var txtbox = document.getElementById('textbox');
+        var txtval = txtbox.value;
+        event.preventDefault();
 
-    if($.trim($('#textbox').val())) {
-      $('<li class="items"></li>').appendTo('#list').html('<span><img class="basket" src="images/basket.png"/>' + 
-        txtval + '</span><img class="trash" src="images/delete.png"/>');
-      document.getElementById('textbox').value = '';
-    };
-  });
+        if ($.trim($('#textbox').val())) {
+            $('<li class="items"></li>').appendTo('#list').html('<span><img class="basket" src="images/basket.png"/>' +
+                txtval + '</span><img class="trash" src="images/delete.png"/>');
+            document.getElementById('textbox').value = '';
+        };
+    });
 
-// show delete icon over item
-  $('#list').on('mouseenter', 'li', function() {
-    $(this).children('.trash').toggleClass('show');
-  });
-  
-  $('#list').on('mouseleave', 'li', function() {
-    $(this).children('.trash').toggleClass('show');
-  });
+    // show delete icon over item
+    $('#list').on('mouseenter', 'li', function() {
+        $(this).children('.trash').toggleClass('show');
+    });
 
-// remove item from list
-  $('#list').on('click', '.trash', function(e) {
-    e.preventDefault(); 
-    var select_item = $(this).closest('li').text();
-    $(this).parent().remove();
-  });
+    $('#list').on('mouseleave', 'li', function() {
+        $(this).children('.trash').toggleClass('show');
+    });
 
-// remove item add to bought
-  $('#list').on('click', '.basket', function(e) {
-    e.preventDefault(); 
-    var selectItem = $(this).closest('li').text();
-    $('<li class="b_items"></li>').appendTo('#bought').html('<span><img class="check" src="images/check.png"/>' + selectItem + '</span>');
-    $(this).parent().remove();
-    $('.show').remove();
-    $('#list li:empty').remove();
-  });
+    // remove item from list
+    $('#list').on('click', '.trash', function(e) {
+        e.preventDefault();
+        var select_item = $(this).closest('li').text();
+        $(this).parent().remove();
+    });
 
-// sort items
-  $('#list').sortable({ axis: "y" });
+    // remove item add to bought
+    $('#list').on('click', '.basket', function(e) {
+        e.preventDefault();
+        var selectItem = $(this).closest('li').text();
+        $('<li class="b_items"></li>').appendTo('#bought').html('<span><img class="check" src="images/check.png"/>' + selectItem + '</span>');
+        $(this).parent().remove();
+        $('.show').remove();
+        $('#list li:empty').remove();
+    });
 
-// clear list
-  $('#clear-btn').click(function() {
-    $('#list').empty();
-    $('#bought').empty();
-  });
+    // sort items
+    $('#list').sortable({
+        axis: "y"
+    });
+
+    // clear list
+    $('#clear-btn').click(function() {
+        $('#list').empty();
+        $('#bought').empty();
+    });
 
 });
